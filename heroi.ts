663085ffb965aaa56fb839c8518fc0b5b06ed4ee
wildsong550 <wildsong550@gmail.com>
2024-07-24 21:24:44 -0400

    Add files via upload

---
+++ heroi.ts
@@ -0,0 +1,13 @@
+class Heroi{
+    public nome: string = ''
+    forca:number = 100
+    nivel : number = 1
+
+    olharDestemido(){
+    console.log("olhar destemido!!!")
+    }
+
+}
+
+  let Heroil = new Heroi()
+console.log(Heroil)  
---
+++ personagem.ts
@@ -0,0 +1,18 @@
+class Personagem{
+    public nome: string
+    public nivel: number
+    public forca: number
+
+constructor(nome:string,forca:number,vida:number){
+    
+    this.nome = nome
+    this.forca = forca
+    this.nivel = 1
+    this.comportamento = comportamento
+} 
+}
+
+   olhar(){
+    console.log("olhando")
+   }
+
